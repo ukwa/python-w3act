b007cd7976848539bc0c9a936c5745a2e097e229
Roger G. Coram <roger.coram@bl.uk>
2015-05-12 12:02:50 +0100

    Included secret-server.

--- w3act/lib.py
+++ w3act/lib.py
@@ -44,3 +44,7 @@ class ACT():
     def get_by_export(self, frequency):
         return self._get_json("%s/by/%s" % (settings.W3ACT_EXPORT_BASE, frequency))
 
+    def get_secret(self, secret_id):
+        return self._get_json("%s/secret/%s" % (settings.W3ACT_BASE, secret_id))
+        return r.content
+
