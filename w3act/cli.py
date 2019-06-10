ceec725ba8e580a81980bfa3cb87dae8390c7a97
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-06-10 16:04:26 +0100

    Tidy up code.

--- w3act/cli.py
+++ w3act/cli.py
@@ -20,9 +20,8 @@ import os
 import sys
 import logging
 import argparse
-from urlparse import urlparse
-
-from lib.w3act.w3act import w3act
+from urllib.parse import urlparse
+from w3act.w3act import w3act
 
 
 # Set up a logging handler:
@@ -43,59 +42,59 @@ logger.setLevel( logging.INFO )
 
 
 def main():
-	parser = argparse.ArgumentParser('Interrogate the W3ACT API.')
-	parser.add_argument('-w', '--w3act-url', dest='w3act_url', 
-					type=str, default="http://localhost:9000/act/", 
-					help="W3ACT endpoint to use [default: %(default)s]" )
-	parser.add_argument('-u', '--w3act-user', dest='w3act_user', 
-					type=str, default="wa-sysadm@bl.uk",
-					help="W3ACT user email to login with [default: %(default)s]" )
-	parser.add_argument('-p', '--w3act-pw', dest='w3act_pw', 
-					type=str, default="sysAdmin", 
-					help="W3ACT user password [default: %(default)s]" )
-	parser.add_argument('action', metavar='action', help="The action to perform (one of 'add-target', 'list-targets', 'get-target').")
-	
-	args, subargs = parser.parse_known_args()
-	
-	# Connect
-	act = w3act(args.w3act_url,args.w3act_user,args.w3act_pw)
-	
-	if args.action == "list-targets":
-		json = act.get_json("api/targets")
-		print json
-	elif args.action == 'add-target':
-		r = act.post_target(subargs[0], subargs[1])
-		print r.status_code
-		print r.text		
-	elif args.action == 'update-target-schedule':
-		r = act.update_target_schedule(int(subargs[0]), subargs[1], subargs[2])
-		print r.status_code
-		print r.text
-	elif args.action == 'set-selector':
-		r = act.update_target_selector(int(subargs[0]))
-		print r.status_code
-		print r.text
-	elif args.action == 'watch-target':
-		r = act.watch_target(int(subargs[0]))
-		print r.status_code
-		print r.text
-	elif args.action == 'unwatch-target':
-		r = act.unwatch_target(int(subargs[0]))
-		print r.status_code
-		print r.text
-	elif args.action == 'add-document':
-		doc = {}
-		wtid = subargs[0]
-		doc['target_id'] = int(wtid)
-		doc['wayback_timestamp'] = subargs[1]
-		doc['document_url'] = subargs[2]
-		doc['landing_page_url'] = subargs[3]
-		doc['filename'] = os.path.basename( urlparse(doc['document_url']).path )
-		doc['size'] = ""
-		logger.debug("Sending doc: %s" % doc)
-		r = act.post_document(doc)
-		print r.status_code
-		print r.text
+    parser = argparse.ArgumentParser('Interrogate the W3ACT API.')
+    parser.add_argument('-w', '--w3act-url', dest='w3act_url',
+                    type=str, default="http://localhost:9000/act/",
+                    help="W3ACT endpoint to use [default: %(default)s]" )
+    parser.add_argument('-u', '--w3act-user', dest='w3act_user',
+                    type=str, default="wa-sysadm@bl.uk",
+                    help="W3ACT user email to login with [default: %(default)s]" )
+    parser.add_argument('-p', '--w3act-pw', dest='w3act_pw',
+                    type=str, default="sysAdmin",
+                    help="W3ACT user password [default: %(default)s]" )
+    parser.add_argument('action', metavar='action', help="The action to perform (one of 'add-target', 'list-targets', 'get-target').")
+
+    args, subargs = parser.parse_known_args()
+
+    # Connect
+    act = w3act(args.w3act_url,args.w3act_user,args.w3act_pw)
+
+    if args.action == "list-targets":
+        json = act.get_json("api/targets")
+        print(json)
+    elif args.action == 'add-target':
+        r = act.post_target(subargs[0], subargs[1])
+        print(r.status_code)
+        print(r.text		)
+    elif args.action == 'update-target-schedule':
+        r = act.update_target_schedule(int(subargs[0]), subargs[1], subargs[2])
+        print(r.status_code)
+        print(r.text)
+    elif args.action == 'set-selector':
+        r = act.update_target_selector(int(subargs[0]))
+        print(r.status_code)
+        print(r.text)
+    elif args.action == 'watch-target':
+        r = act.watch_target(int(subargs[0]))
+        print(r.status_code)
+        print(r.text)
+    elif args.action == 'unwatch-target':
+        r = act.unwatch_target(int(subargs[0]))
+        print(r.status_code)
+        print(r.text)
+    elif args.action == 'add-document':
+        doc = {}
+        wtid = subargs[0]
+        doc['target_id'] = int(wtid)
+        doc['wayback_timestamp'] = subargs[1]
+        doc['document_url'] = subargs[2]
+        doc['landing_page_url'] = subargs[3]
+        doc['filename'] = os.path.basename( urlparse(doc['document_url']).path )
+        doc['size'] = ""
+        logger.debug("Sending doc: %s" % doc)
+        r = act.post_document(doc)
+        print(r.status_code)
+        print(r.text)
 
 if __name__ == "__main__":
-	main()
+    main()
